edd66c51d194f436409ce9a5315e999a6dd63a33
Ruth <141682882+Ruth-cell238@users.noreply.github.com>
2024-01-02 01:51:17 -0500

    Add files via upload

---
+++ 18_F_Print.py
@@ -0,0 +1,8 @@
+var1 = "Hello"
+var2 = "World"
+var3 = 2023
+#print(var1 + " " + var2 + "! Its " + str(var3))
+#print(var1, var2, "! Its ", var3)
+print(f"{var1} {var2}! Its {var3}! It is a great year, I'm so happy to be " "learning python in the cohoart.")
+pi = 3.1415926535
+print(f"The value of pi is {pi:.2f}")
