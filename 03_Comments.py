b37866481ffc066c8a5c0f916e7dc98457e5f7fb
Ruth <141682882+Ruth-cell238@users.noreply.github.com>
2024-01-02 01:34:24 -0500

    Add files via upload

---
+++ 03_Comments.py
@@ -0,0 +1,21 @@
+print("This is about comments")
+# In a python, just like social media, everything is ingnored after the #
+# This line will be ignored too
+#print("I don't want this line to run")
+'''
+ikhjhu
+lkjhu
+'''
+"""
+bjhhj
+"""
+'''
+This is commented out
+so is this
+and this too
+'''
+"""
+This is commented out
+so is this
+and this too
+"""
