cbfa5ba10ddb045ef00beeee3c9797a267225183
Ruth <141682882+Ruth-cell238@users.noreply.github.com>
2024-01-02 01:37:26 -0500

    Add files via upload

---
+++ 05_Numbers.py
@@ -0,0 +1,14 @@
+#num1 = 0.1
+#num2 = 0.9
+#print(num1, num2)
+#num1 = int(num1)
+#num2 = int(num2)
+#print(num1, num2)
+#num1 = 1
+#print(float(num1))
+#num1 = 0.1
+#num2 = 0.9
+#print(round(num1), round(num2))
+num1 = 0.11345
+num2 = 0.12678
+print(round(num1, 2), round(num2, 2))
