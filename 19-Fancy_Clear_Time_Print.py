c6f40a4305330fd20f9c93fbbd7ba1f662c8bf6c
Ruth <141682882+Ruth-cell238@users.noreply.github.com>
2024-01-02 01:51:53 -0500

    Add files via upload

---
+++ 19-Fancy_Clear_Time_Print.py
@@ -0,0 +1,18 @@
+import os
+import time
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+clear()
+print("Its time we get theis party started.\n"
+      "Get everybody to start together.\n"
+      "Ok 3...", end="", flush=True)
+time.sleep(1)
+print("2...", end="", flush=True)
+time.sleep(1)
+print("1...", end="", flush=True)
+time.sleep(1)
+print(" Lets Jam!")  
+time.sleep(3)
+print("Good Bye")
+time.sleep(1)
+clear()
