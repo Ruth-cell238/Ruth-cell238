4428825d8d55a966525fc73c61944e34fcdabb6b
Ruth <141682882+Ruth-cell238@users.noreply.github.com>
2024-01-02 01:53:31 -0500

    Add files via upload

---
+++ 16_Import.py
@@ -0,0 +1,12 @@
+import random
+num = random.randint(1,10)
+print(num)
+import random as r
+num = r.randint(1,10)
+print(num)
+from random import randint
+num = randint(1,10)
+print(num)
+from random import randint as rand
+num = rand(1,10)
+print(num)
