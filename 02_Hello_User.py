79596cfdac714ae7b43245c68cfceda51b643d07
Ruth <141682882+Ruth-cell238@users.noreply.github.com>
2024-01-02 01:32:20 -0500

    Add files via upload

---
+++ 02_Hello_User.py
@@ -0,0 +1,11 @@
+print("Hello World")
+var1 = "Hello"
+var2 = "World"
+print(var1 + var2)
+var1 = "Hello "
+print(var1 + var2)
+var1 = "Hello"
+print(var1 + " " + var2 + "!")
+name = input("What is your name?")
+print("name")
+print("Hello " + name)
