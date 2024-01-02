935f44bdb78d7b7cf381e477b55d123197824e66
Ruth <141682882+Ruth-cell238@users.noreply.github.com>
2024-01-02 01:29:33 -0500

    Add files via upload

---
+++ 01_Activity.py
@@ -0,0 +1,6 @@
+q1 = input ("What is your favorite color? ")
+q2 = input ("What is your favorite operating system? ")
+q3 = input ("what is your favorite sport team? ")
+q4 = input ("what is smell? ")
+print(" There once was a person who had a " + q1 + " laptop, the fans emitted a scent of " + q4 + " It hung around like " + q2 + " It was adorned with " + q3 +" stricks " )
+
