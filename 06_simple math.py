60417d774b51caa37397188e50664d0a6a6c5108
Ruth <141682882+Ruth-cell238@users.noreply.github.com>
2024-01-02 01:37:05 -0500

    Add files via upload

---
+++ 06_simple math.py
@@ -0,0 +1,9 @@
+num1 = 33
+num2 = 4
+num3 = num1 + num2
+num4 = num1 - num2
+num5 = num1 * num2
+num6 = num1 / num2
+num7 = num1 ** num2
+num8 = num1 % num2
+print(num3, num4, num5, num6, num7, num8)
