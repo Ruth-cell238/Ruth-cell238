d227a7e6e06171fa71c8ce1bcd03972becbadef0
Ruth <141682882+Ruth-cell238@users.noreply.github.com>
2024-01-02 01:39:14 -0500

    Add files via upload

---
+++ 09_Method.py
@@ -0,0 +1,5 @@
+#var = input("Give me some text, sssshh be quite").lower()
+#var = input("Give me some text, it's about to get lound: ").upper()
+print("var")
+var = input("Give me some text, it's abount to get weird: ").lower
+print(var)
